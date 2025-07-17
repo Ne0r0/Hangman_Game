bc85fd9f5dff5aff62676a7f3e02fe845a2e9b66
Ne0r0 <buflinskis@gmail.com>
2025-07-17 19:48:46 +0300

    Added list of words

---
+++ words.py
@@ -0,0 +1,8 @@
+python_terms = [
+    "tuple", "class", "float", "input", "print", "while", "break", "async", "await",
+    "super", "global", "lambda", "import", "return", "object", "method", "string",
+    "format", "append", "remove", "insert", "extend", "sorted", "filter", "reduce",
+    "random", "choice", "logging", "except", "finally", "raise", "decode", "encode",
+    "module", "syntax", "indent", "kwargs", "args", "yield", "define", "update",
+    "values", "items", "length", "output", "errors", "binary", "branch", "commit"
+]
